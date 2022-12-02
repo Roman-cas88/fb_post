bf6e5efa893e2502216dc7e1db94bfdad157ed82
Roman-cas88 <orekhovskiy.roman@gmail.com>
2022-12-02 20:43:47 +0100

    create feadback field

--- src/feedback/feedback.jsx
+++ src/feedback/feedback.jsx
@@ -9,6 +9,9 @@ import avatarStiker from './icons/avatarStiker.png'
 import gif from './icons/gif.png'
 import emoji from './icons/emoji.png'
 import stiker from './icons/stiker.png'
+import randomAvatar from './icons/randomAvatar.jpg'
+import diamond from './icons/diamond.png'
+import commentLike from './icons/commentLike.png'
 
 export const Feedback = () => {
 
@@ -42,13 +45,29 @@ export const Feedback = () => {
                 <div className="filter"> <span>Most relevant <i>▼</i></span></div>
                 <div className="writeField">
                     <img src={myAvatar} alt="Avatar" id="avatarImg" />
-                        <input type="text" placeholder='Write a comment...'/>
-                        <img src={avatarStiker} alt="avatarStiker" className="avatarStiker"/>
-                        <img src={emoji} alt="emoji" className="emoji" />
-                        <img src={gif} alt="gif" className="gif" />
-                        <img src={stiker} alt="stiker" className="stiker" />
+                    <input type="text" placeholder='Write a comment...'/>
+                    <img src={avatarStiker} alt="avatarStiker" className="avatarStiker"/>
+                    <img src={emoji} alt="emoji" className="emoji" />
+                    <img src={gif} alt="gif" className="gif" />
+                    <img src={stiker} alt="stiker" className="stiker" />
+                </div>
+
+                <div className="info">Press Enter to post.</div>
+
+                <div className="randomComment">
+                    <img src={randomAvatar} alt="Avatar" className="randomAvatar" />
+                    <div className="topComment">
+                        <span className="topFun">
+                            <img src={diamond} alt="diamond" className="diamond" />
+                            Top fan
+                        </span>
+                        <p className="nikname">Yaw Nana Walter</p>  
+                        <div className="commentContent"><img src={commentLike} alt="heart" /></div> 
+                        <p className="comentFeedback"><span>Like</span> <span>Reply</span> <span id="timeRemind">1w</span></p>
+                    </div>
                 </div>
             </div>
+            <div className="commentInfo">Most Relevant is selected, so some comments may have been filtered out.</div>
         </div>
     )
 }
