cb3bdbd9d2c101eb5b383f306c69560b3724c6bb
Roman-cas88 <orekhovskiy.roman@gmail.com>
2022-12-02 15:07:18 +0100

    create feedback content

--- src/feedback/feedback.jsx
+++ src/feedback/feedback.jsx
@@ -4,11 +4,17 @@ import thumbsUp from './icons/thumbsUp.png'
 import butLike from './icons/butLike.png'
 import butComment from './icons/butComment.png'
 import butShare from './icons/butShare.png'
+import myAvatar from './icons/myAvatar.jpg'
+import avatarStiker from './icons/avatarStiker.png'
+import gif from './icons/gif.png'
+import emoji from './icons/emoji.png'
+import stiker from './icons/stiker.png'
 
 export const Feedback = () => {
 
     return (
         <div className="feedback">
+
             <div className="underVideo">
                 <img src={likeIcon} alt="like" className="likeIcon" />
                 <img src={thumbsUp} alt="thumbsUp" className="thumbsUp" />
@@ -16,6 +22,7 @@ export const Feedback = () => {
                 <a href="#comments" className="comments">23 Comments</a>
                 <a href="#shares" className="shares">8 Shares</a>
             </div>
+
             <div className="buttons">
                 <div className="butLike">
                     <img src={butLike} alt="Like" className="butLikeImg" />
@@ -28,6 +35,18 @@ export const Feedback = () => {
                 <div className="butShare">
                     <img src={butShare} alt="Share" className="butShareImg" />
                     <span>Share</span>
+                </div>
+            </div>
+
+            <div className="commentField">
+                <div className="filter"> <span>Most relevant <i>▼</i></span></div>
+                <div className="writeField">
+                    <img src={myAvatar} alt="Avatar" id="avatarImg" />
+                        <input type="text" placeholder='Write a comment...'/>
+                        <img src={avatarStiker} alt="avatarStiker" className="avatarStiker"/>
+                        <img src={emoji} alt="emoji" className="emoji" />
+                        <img src={gif} alt="gif" className="gif" />
+                        <img src={stiker} alt="stiker" className="stiker" />
                 </div>
             </div>
         </div>
